f001ef88082619f39721bc1ba1eafa3d88717f0f
Anthony Steven Quito Penadillo <anthony.quito@tecsup.edu.pe>
2021-12-02 23:04:02 -0500

    first commit

---
+++ src/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'ec2-3-84-148-45.compute-1.amazonaws.com',
+        user: 'anthony',
+        password: '12345678a',
+        database: 'crud'
+    }
+
+};
